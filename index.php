fdcd68d64b0e398a41c8ca60bffba7242eb9bd86
nancy0625 <1308013168@qq.com>
2017-02-27 13:06:42 +0800

    Hello my first PHP project!

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+echo "00";
+test01();
+function test01(){
+    echo "xxx";
+    echo "dddd";
+}
+$a = 1;
+$b = 2;
+$c =3;
+class hello{
+    public function n01(){
+        echo "hello1";
+    }
+    protected function no2(){
+        echo "hello2";
+    }
+    private function no3(){
+        
+    }
+}
+?>
